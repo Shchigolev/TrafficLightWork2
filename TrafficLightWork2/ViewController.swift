d244880b9bb95b163835ab88ce2f37d0b589d4da
Shchigolev <shchigolevvv@gmail.com>
2022-09-16 16:26:23 +0300

    add enum and func

--- TrafficLightWork2/ViewController.swift
+++ TrafficLightWork2/ViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet var redLight: UIView!
@@ -14,7 +18,25 @@ class ViewController: UIViewController {
     @IBOutlet var greenLight: UIView!
 
     @IBOutlet var startButton: UIButton!
-
+    
+    private var currentLight = CurrentLight.red
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        startButton.layer.cornerRadius = 10
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
+    }
+    
     @IBAction func startButtonChange() {
     }
 }
